542d49b0139d31df2e0fcdd549a083a01cedc69f
chirsworking <chrisjorgework@protonmail.com>
2019-11-04 20:25:48 +0200

    Keep login and registration boxes aligned to top

--- client/src/modules/login/LoginBox.tsx
+++ client/src/modules/login/LoginBox.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-import styled from "../../config/styles";
+import styled, { mq } from "../../config/styles";
 
 const Wrapper = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
   min-height: 100vh;
 `;
 
 const Box = styled.div`
+  margin-top: 2rem;
   padding: 1.5rem;
   border-radius: 4px;
   min-height: 350px;
   width: 350px;
+  ${mq("medium")} {
+    margin-top: 10vh;
+  }
 `;
 
 const LoginBox: React.FC = ({ children }) => {
