b3bc931de1592d2c9ad8edc813a6c7efd486a084
chirsworking <chrisjorgework@protonmail.com>
2019-12-03 19:04:45 +0200

    Add route-based code splitting

--- client/src/config/routes.tsx
+++ client/src/config/routes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,40 +6,42 @@ import {
   Switch
 } from "react-router-dom";
 
+import { Spinner } from "../components/common";
 import { AuthRoute } from "../components/login";
-import {
-  PageNotFoundPage,
-  HomePage,
-  LoginPage,
-  RegistrationPage,
-  SettingsPage,
-  ProjectPage
-} from "../pages";
+
+const PageNotFoundPage = lazy(() => import("../pages/PageNotFoundPage"));
+const HomePage = lazy(() => import("../pages/HomePage"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const ProjectPage = lazy(() => import("../pages/ProjectPage"));
+const SettingsPage = lazy(() => import("../pages/SettingsPage"));
+const RegistrationPage = lazy(() => import("../pages/RegistrationPage"));
 
 const Routes = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/login" exact>
-          <LoginPage />
-        </Route>
-        <Route path="/registration" exact>
-          <RegistrationPage />
-        </Route>
-        <AuthRoute path="/project/:id" exact>
-          <ProjectPage />
-        </AuthRoute>
-        <AuthRoute path="/settings" exact>
-          <SettingsPage />
-        </AuthRoute>
-        <Redirect from="/project" to="/" exact />
-        <Route path="*">
-          <PageNotFoundPage />
-        </Route>
-      </Switch>
+      <React.Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/login" exact>
+            <LoginPage />
+          </Route>
+          <Route path="/registration" exact>
+            <RegistrationPage />
+          </Route>
+          <AuthRoute path="/project/:id" exact>
+            <ProjectPage />
+          </AuthRoute>
+          <AuthRoute path="/settings" exact>
+            <SettingsPage />
+          </AuthRoute>
+          <Redirect from="/project" to="/" exact />
+          <Route path="*">
+            <PageNotFoundPage />
+          </Route>
+        </Switch>
+      </React.Suspense>
     </Router>
   );
 };
