32a4be641cc810935164b75cdd7de8619c6c23de
chirsworking <chrisjorgework@protonmail.com>
2019-12-04 20:42:06 +0200

    Add ErrorBoundary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,22 +9,25 @@ import { useMeQuery } from "./components/login/login.requests";
 import { client } from "./config/apollo";
 import { SideMenuProvider } from "./utils/SideMenuProvider";
 import "./config/i18n";
+import ErrorBoundary from "./utils/ErrorBoundary";
 
 const App: React.FC = () => {
   const { data, refetch, loading } = useMeQuery(client);
   const user = data ? data.me : null;
 
   return (
-    <ApolloProvider client={client}>
-      <AuthContext.Provider value={{ user, loading, refetchUser: refetch }}>
-        <ThemeProvider>
-          <SideMenuProvider>
-            <ToastContainer></ToastContainer>
-            <Routes />
-          </SideMenuProvider>
-        </ThemeProvider>
-      </AuthContext.Provider>
-    </ApolloProvider>
+    <ErrorBoundary>
+      <ApolloProvider client={client}>
+        <AuthContext.Provider value={{ user, loading, refetchUser: refetch }}>
+          <ThemeProvider>
+            <SideMenuProvider>
+              <ToastContainer></ToastContainer>
+              <Routes />
+            </SideMenuProvider>
+          </ThemeProvider>
+        </AuthContext.Provider>
+      </ApolloProvider>
+    </ErrorBoundary>
   );
 };
 
---
+++ client/src/utils/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styled from "../config/styles";
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<{}, State> {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <Wrapper>Unfortunately something went wrong. 😓</Wrapper>;
+    }
+
+    return this.props.children;
+  }
+}
+
+const Wrapper = styled.div`
+  color: #666;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+`;
+
+export default ErrorBoundary;
